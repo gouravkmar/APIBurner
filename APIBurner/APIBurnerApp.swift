0e42c8ed5e0b3b6b79707b48b9cb990d23cb51e8
Gourav Kumar <gourav.kumar@saavn.com>
2025-04-25 21:50:43 +0530

    Initial Commit

---
+++ APIBurner/APIBurnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  APIBurnerApp.swift
+//  APIBurner
+//
+//  Created by Gourav Kumar on 25/04/25.
+//
+
+import SwiftUI
+
+@main
+struct APIBurnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ APIBurnerTests/APIBurnerTests.swift
@@ -0,0 +1,17 @@
+//
+//  APIBurnerTests.swift
+//  APIBurnerTests
+//
+//  Created by Gourav Kumar on 25/04/25.
+//
+
+import Testing
+@testable import APIBurner
+
+struct APIBurnerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
